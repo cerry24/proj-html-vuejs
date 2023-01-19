580fb58e02ac78ae5f3aeb158fc3d0138b77674b
cerry24 <samuelecerretti@gmail.com>
2023-01-19 19:18:46 +0100

    implement section decorative images

--- src/components/AppMain components/SectionHowwework.vue
+++ src/components/AppMain components/SectionHowwework.vue
@@ -7,11 +7,21 @@ export default {
 <template>
     <section id="how-we-work">
         <div class="main-container">
-            <div class="video-thumbnail"> <!--thumb with link for youtube video-->
-                <img src="../../assets/img/stock-full-hd-11-670x450.jpg" alt="Youtube video thumbnail">
+            <div class="video-thumbnail"> <!--thumb with link for youtube video & decoration pattern images-->
+                <img src="../../assets/img/stock-full-hd-11-670x450.jpg" alt="Youtube video thumbnail"
+                    class="thumbnail">
+
+                <a href="#" class="youtube-logo">
+                    <img src="../../assets/img/icon-youtube-play.png" alt="youtube logo">
+                </a>
+
+                <img src="../../assets/img/maxcoach-shape-07-150x150.png" alt="square pattern image" class="squares">
+                <img src="../../assets/img/maxcoach-shape-05-150x150.png" alt="square pattern image"
+                    class="round-squares">
             </div>
 
             <div class="text"> <!--right part of the section with title and paragraph-->
+                <div class="pink-circle"></div>
                 <p class="section-intro">how we work</p>
                 <h2>upgrade your skills <span>upgrade your life</span></h2>
                 <p>
@@ -35,24 +45,57 @@ export default {
 
 #how-we-work {
     color: $secondary-font-color;
-    background-color: $main-multi-color;
 }
 
 .main-container {
     justify-content: space-between;
+    align-items: flex-end;
 }
 
 .video-thumbnail {
     width: 50%;
+    position: relative;
+}
+
+.thumbnail {
+    width: 100%;
+    border-radius: 5px;
+}
+
+.youtube-logo {
+    display: block;
+    width: 80px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
     img {
         width: 100%;
-        border-radius: 5px;
     }
+}
+
+.pink-circle {
+    margin-bottom: 50px;
+}
+
+.squares {
+    z-index: -1;
+    position: absolute;
+    bottom: -85px;
+    right: -15px;
+}
+
+.round-squares {
+    z-index: -1;
+    position: absolute;
+    top: 35px;
+    left: -92px;
 }
 
 .text {
     width: 35%;
+    margin-bottom: 50px;
 
     h2 {
         font-size: 30px;
