917b9bd262bb254957e2885fd1583484a2518a1c
cerry24 <samuelecerretti@gmail.com>
2023-01-20 15:07:11 +0100

    implement section decorative images

--- src/components/AppMain components/SectionTransform.vue
+++ src/components/AppMain components/SectionTransform.vue
@@ -7,9 +7,19 @@ export default {
 <template>
     <section id="transform">
         <div class="main-container">
+            <img src="../../assets/img/maxcoach-shape-07-150x150.png" alt="Ornamental square pattern"
+                class="square-pattern"><!--ornamental image-->
+            <img src="../../assets/img/maxcoach-shape-05-150x150.png" alt="square pattern image" class="round-squares">
+            <!--ornamental image-->
+            <img src="../../assets/img/maxcoach-shape-01.png" alt="triple lines pattern image" class="triple-lines">
+            <!--ornamental image-->
+
             <div class="image"><!--image of four people on the left side-->
                 <img src="../../assets/img/home-3-team-image.png" alt="rapresentative image of a team">
             </div>
+
+            <div class="pink-circle"></div> <!--pink circle decoration-->
+
             <div class="text"> <!--title of the section & quick paragraph for introduction-->
                 <p class="section-intro">teverything in <span>maxcoach</span></p>
                 <h2>we're here to <span>transform</span>you!</h2>
@@ -29,11 +39,31 @@ export default {
 #transform {
     background-color: $main-multi-color;
     color: $secondary-font-color;
+    position: relative;
+}
+
+.square-pattern {
+    position: absolute;
+    bottom: -17%;
+    left: 34.5%;
+}
+
+.round-squares {
+    position: absolute;
+    top: 30%;
+    left: 18%;
+}
+
+.triple-lines {
+    position: absolute;
+    bottom: 9%;
+    left: 6%;
 }
 
 .image {
     width: 60%;
     text-align: center;
+    z-index: 1;
 }
 
 .text {
