ce531c50380609526aeeb0276bc292fcadbc8fd6
cerry24 <samuelecerretti@gmail.com>
2023-01-20 11:22:21 +0100

    implement front review card

--- src/components/AppMain components/SectionTestimonials.vue
+++ src/components/AppMain components/SectionTestimonials.vue
@@ -22,8 +22,24 @@ export default {
                 </p>
             </div>
 
-            <div class="reviews-cards">
+            <div class="reviews-cards"> <!--testimonials cards container-->
+                <div class="front-card"> <!--close up card of testimonial-->
+                    <h3>Professional team of specialists and passionate mentors at reach</h3>
+                    <p>
+                        I need to get a certification fo English proficiency and MaxCoach is my best choice. Their
+                        tutors are smart and professional when dealing with students.
+                    </p>
+                    <div class="reviewer-info">
+                        <figure>
+                            <img src="../../assets/img/testimonial-avata-01.jpg" alt="testimonial profile picture">
+                        </figure>
 
+                        <div>
+                            <h4>madley pondor</h4>
+                            <p>/IT Specialist</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -35,6 +51,10 @@ export default {
 #testimonials {
     background-color: $main-multi-color;
     color: $secondary-font-color;
+}
+
+.main-container {
+    justify-content: space-between;
 }
 
 .text {
@@ -67,5 +87,56 @@ p {
 .more-info a {
     color: $main-font-color;
     font-weight: bold;
+}
+
+.reviews-cards {
+    width: 50%;
+}
+
+.front-card {
+    padding: 35px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 5px;
+
+    h3 {
+        color: $main-font-color;
+        font-size: 24px;
+        margin-bottom: 12px;
+    }
+
+    p {
+        margin-bottom: 35px;
+    }
+}
+
+.reviewer-info {
+    display: flex;
+
+    figure {
+        width: 60px;
+        height: 60px;
+        overflow: hidden;
+        border-radius: 50%;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    div {
+        margin-left: 15px;
+
+        h4 {
+            color: $main-font-color;
+            text-transform: uppercase;
+            margin-bottom: 8px;
+        }
+
+        p {
+            margin: 0;
+        }
+    }
 }
 </style>
